3720232cb782c1d355aef73e09d1c0d7e08eb479
oznur ozen <oznurozen67@gmail.com>
2021-08-22 14:08:02 +0300

    contact book app

--- app/src/main/java/com/ozen/contactbookapp/Kisiler.java
+++ app/src/main/java/com/ozen/contactbookapp/Kisiler.java
@@ -1,8 +1,11 @@
 package com.ozen.contactbookapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import java.util.Arrays;
 public class Kisiler extends Activity {
 
     ListView kisilerListe;
+    Button Ayse;
 
     private String[] kisiIsimleri={"Ayse","Mehmet","Ali","Zeynep","Beyza","Zuleyha","Hamza"};
 
@@ -22,10 +26,22 @@ public class Kisiler extends Activity {
 
        kisilerListe = findViewById(R.id.kisilerListe);
 
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , android.R.id.text1, kisiIsimleri);
         Arrays.sort(kisiIsimleri);
         Arrays.toString(kisiIsimleri);
         kisilerListe.setAdapter(adapter);
+
+        islemler();
+
     }
+    void islemler(){
+        Ayse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Kisiler.this,Ayse.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
